8cc7f4fe8b187d92d864311ead9af5761531c0c6
Vinícius Amirat <viniciusamirat39@gmail.com>
2023-06-10 01:18:04 -0300

    add new validation in user creation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -9,12 +9,27 @@ const createUser = async (req, res)=>{
     password = String(password)
     name = String(name).trim().toLowerCase()
 
+    const emailInUse = await getUserByEmail(email)
+    if(emailInUse){
+      return res.status(400).json({message: "This e-mail is alread in use."})
+    }
+
     const salt = await bcrypt.genSalt(12)
     const passHash = await bcrypt.hash(password, salt)
 
     await userModel.createUser(email, passHash, name)
 
     return res.status(201).json({message: "The user was created"})
+  } catch (error) {
+    console.log(error.message)
+    return res.status(500).json()
+  }
+}
+
+const getUserByEmail = async (email)=>{
+  try {
+    const userExists = await userModel.getUserByEmail(email)
+    return userExists
   } catch (error) {
     console.log(error.message)
     return res.status(500).json()
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,11 +2,19 @@ const db = require('./connection')
 
 const createUser = async (email, pass, name)=>{
 	const createdUser = await db.query(`
-		insert into ec_users (email, password, name) values ('${email}', '${pass}', '${name}')
+		insert into public.ec_users (email, password, name) values ('${email}', '${pass}', '${name}')
 	`)
 	return createdUser
 }
 
+const getUserByEmail = async (email)=>{
+	const user = await db.query(`
+		select exists(select true from public.ec_users where email = '${email}')
+	`)
+	return user.rows[0].exists
+}
+
 module.exports = {
-	createUser
+	createUser,
+	getUserByEmail
 }
