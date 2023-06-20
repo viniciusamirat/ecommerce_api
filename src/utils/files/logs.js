9580fa0a44934469f6cc1019390f7265c345bce3
Vinícius Amirat <viniciusamirat39@gmail.com>
2023-06-19 21:10:20 -0300

    adding new function for writing error logs

---
+++ src/utils/files/logs.js
@@ -0,0 +1,24 @@
+const { appendFile } = require('fs')
+
+const writeLog = (fileName, content)=>{
+  const utcDate = new Date().toUTCString()
+
+  const newContent = `"${content}" - ${utcDate}\n`
+
+  return new Promise((resolve, reject)=>{
+
+    appendFile(fileName, newContent, (err)=>{
+
+      if (err) return reject(err)
+
+      return resolve()
+
+    })
+
+  })
+
+}
+
+module.exports = {
+  writeLog
+}
