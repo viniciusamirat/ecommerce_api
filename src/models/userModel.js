6616d69364417b7ad95cf4b884e18c9c83266e12
Vinícius Amirat <viniciusamirat39@gmail.com>
2023-06-20 23:34:27 -0300

    adding new functions for queries

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,16 +2,17 @@ const db = require('./connection')
 
 const createUser = async (email, pass, name)=>{
 	const createdUser = await db.query(`
-		insert into public.ec_users (email, password, name) values ('${email}', '${pass}', '${name}')
+		select * from public.fc_create_user('${email}', '${pass}', '${name}')
 	`)
 	return createdUser
 }
 
 const getUserByEmail = async (email)=>{
 	const user = await db.query(`
-		select exists(select true from public.ec_users where email = '${email}')
+		select * from fc_get_user_by_email('${email}')
 	`)
-	return user.rows[0].exists
+	
+	return user.rows[0].r_exists
 }
 
 const getUserForLogin = async (email, pass)=>{
